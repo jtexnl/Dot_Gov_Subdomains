8914fb1903650494e0ce532fc61e739f13d05737
John Tindel <jtexnl@gmail.com>
2016-03-01 09:53:33 -0500

    Create BS_Scraper.py
    First attempt at a scraper using BeautifulSoup. Includes feature to wait 60 seconds after every 5000 requests to prevent lock-out. Initial run yielded about 30k unique addresses, but requests are still being blocked after a certain point.

---
+++ BS_Scraper.py
@@ -0,0 +1,46 @@
+import urllib2
+from bs4 import BeautifulSoup
+import requests
+from requests.exceptions import HTTPError
+import csv
+import httplib
+import requests_cache
+import time
+
+linkSet = ['http://gsa.gov']
+outputFile= open('scraperOutput.csv', 'w+')
+output_writer=csv.writer(outputFile, lineterminator='\n')
+output_writer.writerow(['url'])
+requests_cache.install_cache('test_cache', backend='sqlite')
+
+counter = 0
+for i in linkSet:
+	request = urllib2.Request(i)
+	try:
+		response = urllib2.urlopen(request)
+		soup = BeautifulSoup(response)
+		for a in soup.findAll('a'):
+			try:
+				if '.gov' in a['href']:
+					if counter % 5000 == 0 and not counter == 0:
+						time.sleep(60)
+					if a['href'] not in linkSet:
+						if not str(a['href']).startswith("mailto"):
+							linkSet.append(a['href'])
+							outputLink = str(a['href'])
+							output_writer.writerow([outputLink])
+							counter += 1
+							print("processed link %i" %(counter))
+						else:
+							continue
+			except KeyError:
+				continue
+	except urllib2.HTTPError, e:
+		continue
+	except urllib2.URLError, e:
+		continue
+	except ValueError:
+		continue
+	except httplib.BadStatusLine, e:
+		continue
+print('scan complete')
